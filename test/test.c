1970e00dfa506a3cc494e9b14bac99f6e8e50fef
Frank Thaule <frankthaule@outlook.com>
2025-04-05 09:03:47 +0200

    Refactoring

--- test/test.c
+++ test/test.c
@@ -3,8 +3,9 @@
 
 #include "mtxorb.h"
 
-static const struct mtxorb_info_s lcd_info = {
-    MTXORB_LKD,     /* Display type */
+
+static const mtxorb_info_t display_info = {
+    MTXORB_LKD,     /* Module type */
     20,             /* Display columns */
     4,              /* Display rows */
     5,              /* Display cell-width */
@@ -17,49 +18,47 @@ static MTXORB *lcd = NULL;
 
 static void handle_signal(int sig);
 
+//--------------------------------------------------------------------------------
+
 int main(void)
 {
     char key;
 
     const char some_buffer[] = { 0x43, 0x6f, 0x66, 0x66, 0x65, 0x65 };
-    const char custom_char[] = { 0x00, 0x0A, 0x1F, 0x1F, 0x0E, 0x04, 0x00, 0x00 };
-    /* Bit assignment, i.e. 0b00000 is not allowed in c89, so we use hex representation */
+    const char custom_char[] = { 0x00, 0x0A, 0x1F, 0x1F, 0x0E, 0x04, 0x00, 0x00 };    
 
     /* Capture Ctrl-C and kill signals */
     signal(SIGINT, handle_signal);
     signal(SIGTERM, handle_signal);
 
-/*--------------------------------------------------------------------------------*/
 
-    lcd = mtxorb_open(&lcd_info);
-    if (lcd == NULL) {
-        perror("Error opening connection");
+    if ((lcd = mtxorb_open(&display_info)) == NULL)
+    {
+        printf("mtxorb: %s\n", mtxorb_get_error_str());
         return EXIT_FAILURE;
     }
 
     printf("Connection established. Press Ctrl-C to exit.\n");
 
-/*--------------------------------------------------------------------------------*/
 
-    /* Configure keypad */
-    /*
+    /* Setup keypad */
     mtxorb_set_key_auto_tx(lcd, MTXORB_ON);
-    mtxorb_set_key_auto_repeat(lcd, MTXORB_ON);
-    mtxorb_set_key_debounce_time(lcd, 8);
-    mtxorb_set_keypad_brightness(lcd, 20);
-    */
+    mtxorb_set_key_autorepeat_mode(lcd, MTXORB_HOLD);
+    // mtxorb_set_key_debounce_time(lcd, 8);
+    // mtxorb_set_keypad_brightness(lcd, 20);
 
-    /* Configure display */
-    mtxorb_set_bg_color(lcd, 0, 255, 0);
-    mtxorb_set_brightness(lcd, 120);
-    mtxorb_set_contrast(lcd, 128);
+    /* Setup display */
+    mtxorb_backlight_on(lcd, 0);
+    // mtxorb_set_bg_color(lcd, 100, 255, 100);
+    // mtxorb_set_brightness(lcd, 120);
+    // mtxorb_set_contrast(lcd, 128);
     
-    /* Print content of buffer */
+    /* Print content of some buffer */
     mtxorb_set_cursor(lcd, 7, 3);
     mtxorb_write(lcd, some_buffer, sizeof(some_buffer));
     
-    /* Print custom character in bank 0 */
-    mtxorb_set_custom_char(lcd, 0, custom_char);
+    /* Create and print custom character in bank 0 */
+    mtxorb_create_custom_char(lcd, 0, custom_char);
     mtxorb_set_cursor(lcd, 5, 3);
     mtxorb_putc(lcd, 0);
     mtxorb_set_cursor(lcd, 14, 3);
@@ -69,30 +68,32 @@ int main(void)
     mtxorb_set_cursor(lcd, 3, 1);
     mtxorb_puts(lcd, "System Failure");
 
-    /* Show the cursor */
+    /* Show blinking block cursor */
     mtxorb_set_cursor_block(lcd, MTXORB_ON);
 
-    /* mtxorb_hbar(lcd, 0, 0, 50, MTXORB_RIGHT); */
-    /* mtxorb_vbar(lcd, 0, 32, MTXORB_WIDE); */
-    /* mtxorb_bignum(lcd, 0, 0, 5, MTXORB_LARGE); */
+    // mtxorb_hbar(lcd, 0, 0, 50, MTXORB_RIGHT);
+    // mtxorb_vbar(lcd, 0, 32, MTXORB_WIDE);
+    // mtxorb_bignum(lcd, 0, 0, 5, MTXORB_LARGE);
 
     /* Set GPO's */
-    /* mtxorb_set_output(lcd, MTXORB_GPO1 | MTXORB_GPO3 | MTXORB_GPO5); */
+    // mtxorb_set_output(lcd, MTXORB_GPO1 | MTXORB_GPO3 | MTXORB_GPO5);
+    
 
-/*--------------------------------------------------------------------------------*/
-
-    while (1) {
-        /* Check for avail. input data with a timeout of 100ms */
+    /* Main loop */
+    while(1) {
+        // Check for avail. input data with a timeout of 100ms
         if (mtxorb_read(lcd, &key, 1, 100) > 0)
-            printf("lcd: key pressed '%c' (0x%02X)\n", key, key);
+            printf("Key pressed on display: '%c' (0x%02X)\n", key, key);
     }
 
     return 0;
 }
 
+//--------------------------------------------------------------------------------
+
 static void handle_signal(int signo)
 {
-    (void)signo; /* Omit compile-time warning message */
+    (void)signo; // Omit compile-time warning message
 
     if (lcd) {
         mtxorb_close(lcd);
